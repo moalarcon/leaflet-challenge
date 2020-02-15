c6f3fbb512a0ec46ed1245425ea378089d633ea1
MMA <moalarcon@me.com>
2020-02-15 11:21:58 -0500

    ...

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibW9hbGFyY29uIiwiYSI6ImNrNjhibGM3MjAzZGMzbHBqNGhhM3d4NHEifQ.gN-FicWCcOTqcMJCJJv_iA";
---
+++ static/js/logic.js
@@ -0,0 +1,26 @@
+// Create the tile layer that will be the background of our map
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+});
+
+var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Create the map with our layers
+var map = L.map("map-id", {
+  center: [31.8105728, 132.0110419],
+  zoom: 5,
+  layers: [
+    layers.COMING_SOON,
+    layers.EMPTY,
+    layers.LOW,
+    layers.NORMAL,
+    layers.OUT_OF_ORDER
+  ]
+});
+
+// Add our 'lightmap' tile layer to the map
+lightmap.addTo(map);
+
